4ea9d0973f92752625f0f9af68171d1781f25546
Ankush-Paul-02 <ankushpaulclg2002@gmail.com>
2023-09-28 01:10:43 +0530

    2nd commit

--- src/main/java/com/devmare/blog_app_apis/controllers/UserController.java
+++ src/main/java/com/devmare/blog_app_apis/controllers/UserController.java
@@ -1,14 +1,14 @@
 package com.devmare.blog_app_apis.controllers;
 
+import com.devmare.blog_app_apis.payloads.ApiResponse;
 import com.devmare.blog_app_apis.payloads.UserDTO;
 import com.devmare.blog_app_apis.services.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("/api/users")
@@ -17,10 +17,39 @@ public class UserController {
     @Autowired
     private UserService userService;
 
-    //! http://localhost:8081/api/users/createUser
-    @PostMapping("/createUser")
+    //! http://localhost:8081/api/users/
+    @PostMapping("/")
     public ResponseEntity<UserDTO> createUser(@RequestBody UserDTO userDTO) {
         UserDTO newUser = userService.createUser(userDTO);
         return new ResponseEntity<>(newUser, HttpStatus.CREATED);
+    }
+
+    //! http://localhost:8081/api/users/{userId}
+    @PutMapping("/{userId}")
+    public ResponseEntity<UserDTO> updateUser(@RequestBody UserDTO userDTO, @PathVariable Integer userId) {
+        UserDTO updatedUser = userService.updateUser(userDTO, userId);
+        return ResponseEntity.ok(updatedUser);
+    }
+
+    //! http://localhost:8081/api/users/{userId}
+    @DeleteMapping("/{userId}")
+    public ResponseEntity<ApiResponse> deleteUser(@PathVariable Integer userId) {
+        userService.deleteUser(userId);
+        return new ResponseEntity<>(
+                new ApiResponse("User deleted successfully", true),
+                HttpStatus.OK
+        );
+    }
+
+    //! http://localhost:8081/api/users/
+    @GetMapping("/")
+    public ResponseEntity<List<UserDTO>> getAllUsers() {
+        return ResponseEntity.ok(userService.getAllUsers());
+    }
+
+    //! http://localhost:8081/api/users/
+    @GetMapping("/{userId}")
+    public ResponseEntity<UserDTO> getUserById(@PathVariable Integer userId) {
+        return ResponseEntity.ok(userService.getUserById(userId));
     }
 }
--- src/test/java/com/devmare/blog_app_apis/BlogAppApIsApplicationTests.java
+++ src/test/java/com/devmare/blog_app_apis/BlogAppApIsApplicationTests.java
@@ -1,13 +1,24 @@
 package com.devmare.blog_app_apis;
 
+import com.devmare.blog_app_apis.repositories.UserRepository;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
 class BlogAppApIsApplicationTests {
 
-	@Test
-	void contextLoads() {
-	}
+    @Autowired
+    private UserRepository userRepository;
+
+    @Test
+    void contextLoads() {
+    }
+
+    @Test
+    public void repoTest() {
+        System.out.println("Package name: " + userRepository.getClass().getPackage());
+        System.out.println("Clas name: " + userRepository.getClass().getName());
+    }
 
 }
